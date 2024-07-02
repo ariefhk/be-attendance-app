3e36b914aa2f2a01897887d3e416c5ee3a418794
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2024-07-02 12:47:16 +0700

    chore: dump all project

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { logger } from "./application/logging.js";
-import { web } from "./application/web.js";
-const PORT = process.env.PORT ?? 3000;
-
-web.listen(PORT, () => {
-  logger.info("Listening on port: " + PORT);
-});
